348d8d339ebc666157350c226b2e073d2a282d7b
Ryan Watters <watters.ryan@gmail.com>
2016-08-01 14:28:47 -0500

    removed scrollbars for chrome and ff on pc and updated build script for netlify test

--- build.sh
+++ build.sh
@@ -3,5 +3,3 @@ TODAY=$(date)
 # Build Hugo.
 hugo --ignoreCache --baseURL=https://ryanwatters.io
 cp ${curdir}/public/singles/index.html ${curdir}/public/assets/site-index.json
-open -a Google\ Chrome.app http://localhost:1313;
-hugo serve --buildDrafts=true --buildFuture=true
