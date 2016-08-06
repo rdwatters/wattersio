0f65288bd12905a49d1799860e560e1a4f912eb8
Ryan Watters <watters.ryan@gmail.com>
2016-08-06 12:46:23 -0500

    testing env vars again

--- build-netlify.sh
+++ build-netlify.sh
@@ -1,8 +1,17 @@
 curdir=$(pwd)
 TODAY=$(date)
+branch_name=$(git symbolic-ref --short -q HEAD)
 # Build Hugo.
 
-hugo --ignoreCache --baseURL=https://ryanwatters.io
-echo "Moving PROD site-index.html to assets/site-index.json"
-echo $(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+if branch_name="dev"
+	then
+	hugo --ignoreCache --baseURL=http://dev.ryanwattersio.netlify.com
+	echo "In DEV"
+	else
+	hugo --ignoreCache --baseURL=https://ryanwatters.io
+	echo "In PROD"
+fi
 cp ${curdir}/public/singles/index.html ${curdir}/public/assets/site-index.json
+
+
+# echo $(git rev-parse --abbrev-ref --symbolic-full-name @{u})
