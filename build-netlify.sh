ccd6046f8c6414bddc1595ad8344c1a6ee4136bb
Ryan Watters <watters.ryan@gmail.com>
2016-08-06 12:16:30 -0500

    adding echo to test netlify log

--- build-netlify.sh
+++ build-netlify.sh
@@ -4,4 +4,5 @@ branch_name=$(git symbolic-ref --short -q HEAD)
 echo $branch_name
 # Build Hugo.
 hugo --ignoreCache
+echo "Moving site-index.html to assets/site-index.json"
 cp ${curdir}/public/singles/index.html ${curdir}/public/assets/site-index.json
