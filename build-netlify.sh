3fe0f09aa979480395911acffcdb64f7c29b3dde
Ryan Watters <watters.ryan@gmail.com>
2016-08-06 12:25:07 -0500

    adding echo to test netlify log

--- build-netlify.sh
+++ build-netlify.sh
@@ -2,9 +2,13 @@ curdir=$(pwd)
 TODAY=$(date)
 branch_name=$(git symbolic-ref --short -q HEAD)
 # Build Hugo.
-hugo --ignoreCache
+
 if branch_name="dev"
 	then
-	echo "Moving site-index.html to assets/site-index.json ${branch_name}"
+	hugo --ignoreCache --baseURL=http://dev.ryanwattersio.netlify.com
+	echo "Moving DEV site-index.html to assets/site-index.json"
+	else
+	hugo --ignoreCache --baseURL=https://ryanwatters.io
+	echo "Moving PROD site-index.html to assets/site-index.json"
 fi
 cp ${curdir}/public/singles/index.html ${curdir}/public/assets/site-index.json
