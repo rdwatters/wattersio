dc18ae95ffd5330f8dd0de99bd9a595087351465
Ryan Watters <watters.ryan@gmail.com>
2016-08-06 13:05:26 -0500

    testing env vars again

--- build-netlify.sh
+++ build-netlify.sh
@@ -3,15 +3,22 @@ TODAY=$(date)
 branch_name=$(git symbolic-ref --short -q HEAD)
 # Build Hugo.
 
-if branch_name="dev"
+if [ $branch_name = "dev"]
 	then
 	hugo --ignoreCache --baseURL=http://dev.ryanwattersio.netlify.com
-	echo "In DEV"
+	echo "In DEV - moving singles index.html to site-index.json"
 	else
 	hugo --ignoreCache --baseURL=https://ryanwatters.io
-	echo "In PROD"
+	echo "In PROD - moving singles index.html to site-index.json"
 fi
 cp ${curdir}/public/singles/index.html ${curdir}/public/assets/site-index.json
+
+# onething="yes"
+# twothing="yes"
+# if [ $onething = "yes" ]
+# 	then
+# 	echo "you are correct"
+# fi
 
 
 # echo $(git rev-parse --abbrev-ref --symbolic-full-name @{u})
