a569983c38b3206938579426dba4047f3257442d
Ryan Watters <watters.ryan@gmail.com>
2016-08-06 12:41:50 -0500

    testing env vars again

--- build-netlify.sh
+++ build-netlify.sh
@@ -6,4 +6,5 @@ branch_name=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
 hugo --ignoreCache --baseURL=https://ryanwatters.io
 echo "Moving PROD site-index.html to assets/site-index.json"
 echo ${branch_name}
+echo $branch_name
 cp ${curdir}/public/singles/index.html ${curdir}/public/assets/site-index.json
